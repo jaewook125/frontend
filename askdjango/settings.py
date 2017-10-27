300a8dc7d3ae0f86db012e1bbfedf8cbb6cf87a4
sjw0484 <sjw0484@naver.com>
2017-10-27 16:30:40 +0900

    accounts app 생성

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
     'bootstrap3',
     'rest_framework',
     'easy_thumbnails',
--- askdjango/urls.py
+++ askdjango/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'',include('blog.urls',namespace='blog')),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
