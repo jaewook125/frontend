b1ddaf3deb9227650db2c29619eb1244bc3cc898
sjw0484 <sjw0484@naver.com>
2017-10-27 18:46:48 +0900

    Ajax/Modal을 통한 로그인/회원가입 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView as AuthLoginview
+from django.http import JsonResponse
 from django.views.generic import CreateView
 from django.contrib.auth.decorators import login_required
 
@@ -8,6 +10,34 @@ class SignupFormView(CreateView):
 	form_class = UserCreationForm
 	template_name = 'accounts/signup_form.html'
 	success_url = settings.LOGIN_URL
+
+	def form_valid(self, form):
+		response = super().form_valid(form)
+		if self.request.is_ajax():
+			return JsonResponse({'next_url': self.get_success_url()})
+			#이렇게하면 서버에서 json응답을 받는다
+		return response
+
+	def get_template_names(self):
+		if self.request.is_ajax():
+			return ['accounts/_signup_form.html']
+		return ['accounts/signup_form.html']
+		#템플릿 구별 함수
+
+
+class LoginView(AuthLoginview):
+	def form_valid(self, form):
+		response = super().form_valid(form)
+		if self.request.is_ajax():
+			return JsonResponse({'next_url': self.get_success_url()})
+			#이렇게하면 서버에서 json응답을 받는다
+		return response
+
+	def get_template_names(self):
+		if self.request.is_ajax():
+			return ['accounts/_login.html']
+		return ['accounts/login.html']
+		#템플릿 구별 함수
 
 @login_required
 def profile(request):
